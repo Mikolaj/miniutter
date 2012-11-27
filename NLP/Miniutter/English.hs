d363ecc5367ab9445ad6691e34c938770ef189fc
Mikolaj <mikolaj.konarski@gmail.com>
2012-11-27 12:59:12 +0100

    Flesh out a bit of the library

--- NLP/Miniutter/English.hs
+++ NLP/Miniutter/English.hs
@@ -7,6 +7,7 @@ import Data.Char (toUpper)
 import Data.Text (Text)
 import qualified Data.Text as T
 import NLP.Minimorph.English
+import NLP.Minimorph.Util
 
 -- | Various basic and compound parts of English simple present tense clauses.
 -- Many of the possible nestings do not make sense. We don't care.
@@ -36,7 +37,25 @@ makePhrase :: [Part] -> Text
 makePhrase = T.intercalate (T.singleton ' ') . map makePart
 
 makePart :: Part -> Text
-makePart _ = undefined
+makePart (String t) = T.pack t
+makePart (Text t) = t
+makePart (Cardinal n) = cardinal n
+makePart (Ws p) = defaultNounPlural (makePart p)
+makePart (NWs n p) = T.pack (show n) <+> defaultNounPlural (makePart p)
+makePart (Ordinal n) = ordinal n
+makePart (NthW n p) = undefined
+makePart (AW p) =
+  let t = makePart p
+  in indefiniteDet t <> t
+makePart (WWandW lp) =
+  let i = T.pack "and"
+      lt = map makePart lp
+  in commas i lt  -- TODO: generalize
+makePart (W_sW p_s p) = undefined
+makePart (Compound p1 p2) = (makePart p1) <+> (makePart p2)
+makePart (SubjectVerb s v) = undefined
+makePart (NotSubjectVerb s v) = undefined
+makePart (QSubjectVerb s v) = undefined
 
 -- | Capitalize text.
 capitalize :: Text -> Text
